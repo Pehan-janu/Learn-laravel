82479985a8aac41b7395c3123771e86c0904d6c9
Pehan-janu <pehanjanu@gmail.com>
2025-10-20 00:41:08 +0530

    Update post routes and home logic for user posts
    Modified the home route to display posts only for authenticated users using a user relationship. Added new routes for editing and deleting posts, including GET, PUT, and DELETE endpoints for post management.

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PostController;
 use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
-    $posts = Post::all();
+    $posts = [];
+    //$posts = Post::all();
+   // $posts = Post::where('user_id', Auth::id())->get();
+   if (Auth::check()){
+   $posts = Auth::user()->userCoolPosts()->latest()->get();
+
+   }
     return view('home', ['posts' => $posts]);
 });
 
@@ -18,3 +25,6 @@ Route::post('/login', [UserController::class, 'login']);
 
 //Blog post realed routes
 Route::post('/create-post', [PostController::class, 'createPost' ]);
+Route::get('/edit-post/{post}', [PostController::class, 'showEditScreen']);
+Route::put('/edit-post/{post}', [PostController::class, 'actualyUpdatePost']);
+Route::delete('/delete-post/{post}', [PostController::class, 'deletePost']);
